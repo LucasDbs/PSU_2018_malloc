270323c4facdb57881a5e54ff3dd4c77971574a3
Sabri-Ouaked <sabri.ouaked@epitech.eu>
2019-01-28 12:37:36 +0100

    [EVOLUTION] add block structure

---
+++ malloc.h
@@ -0,0 +1,15 @@
+#if !defined(MALLOC_H)
+#define MALLOC_H
+
+#include <unistd.h>
+
+struct s_block
+{
+    size_t size;
+    struct s_block *next;
+    struct s_block *prev;
+    int	free;
+} block_t;
+
+#endif // MALLOC_H
+
